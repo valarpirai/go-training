769506555e5b511c05dc12e47da76d129c1f63e5
Valarpirai <a.valarpirai@gmail.com>
2017-11-13 18:28:55 +0530

    How to start tutorial

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+var API_KEY = "9f4318e035deb3c6a71f0657a60c42ab"
+
 func main() {
-	println("Hello world!")
+
+	println("Server listening at PORT 8080")
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello!"))
+}
+
+func query(city string) (weatherData, error) {
+	url := "http://api.openweathermap.org/data/2.5/weather?APPID=" + API_KEY + "&q=" + city
+	resp, err := http.Get(url)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
 }
